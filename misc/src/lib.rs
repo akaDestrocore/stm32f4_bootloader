d273e478a0ec0568712905caa0e0927b02c7a43d
akaDestrocore <blackxcat14@yahoo.com>
2025-04-10 23:29:56 +0300

    included newly added modules to `misc\src\lib.rs`

--- misc/src/lib.rs
+++ misc/src/lib.rs
@@ -5,6 +5,8 @@ pub mod ring_buffer;
 pub mod image;
 pub mod flash;
 pub mod systick;
+pub mod bootloader;
+pub mod xmodem;
 
 pub use ring_buffer::RingBuffer;
 pub use systick::{get_tick_ms, wait_ms, setup_systick, increment_tick};
