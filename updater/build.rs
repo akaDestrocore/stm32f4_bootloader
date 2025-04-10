b51d9bc59fbb815f00c2f763b5d78dcc32a67dc1
akaDestrocore <blackxcat14@yahoo.com>
2025-04-10 23:14:20 +0300

    updated `updater` to avoid compilation issues for now

--- updater/build.rs
+++ updater/build.rs
@@ -5,11 +5,20 @@ use std::path::PathBuf;
 
 fn main() {
     println!("cargo:rerun-if-changed=memory.x");
+    println!("cargo:rerun-if-changed=custom.x");
     
-    let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
+    let out: &PathBuf = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
+    
+    // copy memory.x to the output directory
     File::create(out.join("memory.x"))
         .unwrap()
         .write_all(include_bytes!("memory.x"))
+        .unwrap();
+        
+    // create custom.x for our additional sections
+    File::create(out.join("custom.x"))
+        .unwrap()
+        .write_all(include_bytes!("custom.x"))
         .unwrap();
     
     println!("cargo:rustc-link-search={}", out.display());
--- updater/src/main.rs
+++ updater/src/main.rs
@@ -5,23 +5,47 @@ use core::panic::PanicInfo;
 use cortex_m::asm;
 use cortex_m_rt::entry;
 use stm32f4 as pac;
+use misc::image::{ImageHeader, SharedMemory, IMAGE_MAGIC_UPDATER, IMAGE_TYPE_UPDATER};
 
+// symbol from linker
+extern "C" {
+    static __firmware_size: u32;
+}
+
+#[no_mangle]
+#[link_section = ".image_hdr"]
+pub static mut IMAGE_HEADER: ImageHeader = ImageHeader::new(
+    IMAGE_TYPE_UPDATER,
+    IMAGE_MAGIC_UPDATER,
+    1, 0, 0
+);
 
 #[entry]
 fn main() -> ! {
-    // PD12 init 
+    // get firmware size from linker
+    let firmware_size: u32 = unsafe {
+        let size: *const u32 = &__firmware_size as *const u32;
+        let size_value: u32 = *size;
+        
+        // update size in header
+        IMAGE_HEADER.update_data_size(size_value);
+        
+        size_value
+    };
+
+    // PD13 init 
     let peripherals: stm32f4::Peripherals = unsafe { pac::Peripherals::steal() };
     
     // Enable GPIOD clock
     unsafe {
         peripherals.rcc.ahb1enr().modify(|_, w| w.gpioden().set_bit());
 
-        peripherals.gpiod.moder().modify(|_, w| w.moder12().bits(0b01));
-        peripherals.gpiod.otyper().modify(|_, w| w.ot12().clear_bit());
-        peripherals.gpiod.ospeedr().modify(|_, w| w.ospeedr12().bits(0b00));
+        peripherals.gpiod.moder().modify(|_, w| w.moder13().bits(0b01));
+        peripherals.gpiod.otyper().modify(|_, w| w.ot13().clear_bit());
+        peripherals.gpiod.ospeedr().modify(|_, w| w.ospeedr13().bits(0b00));
         
         // enable LED
-        peripherals.gpiod.bsrr().write(|w| w.bs12().set_bit());
+        peripherals.gpiod.bsrr().write(|w| w.bs13().set_bit());
     }
 
     
@@ -34,11 +58,11 @@ fn main() -> ! {
 
 fn toggle_led(peripherals: &pac::Peripherals) {
     unsafe {
-        let current: bool = peripherals.gpiod.odr().read().odr12().bit();
+        let current: bool = peripherals.gpiod.odr().read().odr13().bit();
         if current {
-            peripherals.gpiod.bsrr().write(|w| w.br12().set_bit());
+            peripherals.gpiod.bsrr().write(|w| w.br13().set_bit());
         } else {
-            peripherals.gpiod.bsrr().write(|w| w.bs12().set_bit());
+            peripherals.gpiod.bsrr().write(|w| w.bs13().set_bit());
         }
     }
 }
